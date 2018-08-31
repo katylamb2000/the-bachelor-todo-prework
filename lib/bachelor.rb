83e1743544998858e15142e7edd53bb264ad15c6
katy@karstaway.com <katy@karstaway.com>
2018-08-31 18:44:27 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,28 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data.each do |season, girls|
+    if season.to_s == season
+      girls.each do |contestant_hash|        
+        if contestant_hash[:status] == "Winner"
+          return contestant_hash[:name].split(' ').first
+        end
+    end
+  end
 end
+end
+
 
 def get_contestant_name(data, occupation)
   # code here
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  
+  data.collect do |hometown|
+    if data[:hometown] == hometown
+    data.split (" ")
+    return data.length
+  end
+  end
 end
 
 def get_occupation(data, hometown)
@@ -15,5 +30,6 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  data.each do
+end
 end
